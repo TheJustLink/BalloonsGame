685d84c478ec2d1b7dd45b8e69b061fa37176025
JustLink <linki_mail@mail.ru>
2021-11-15 19:06:30 +0700

    Добавлено лопание шариков пальцем, звук и анимация

--- Assets/Scripts/Balloon/Balloon.cs
+++ Assets/Scripts/Balloon/Balloon.cs
@@ -1,19 +1,39 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 class Balloon : MonoBehaviour
 {
     private Animator _animator;
+    private AudioSource _audioSource;
+
+    private bool _isBlowing;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
+    public void Destroy()
+    {
+        Destroy(gameObject, 1f);
+    }
     public void Blow()
     {
-        _animator.SetTrigger(nameof(Blow));
+        if (_isBlowing) return;
+        _isBlowing = true;
 
-        Destroy(gameObject, 1f);
+        StartBlowAnimation();
+        PlayBlowSound();
+    }
+
+    private void StartBlowAnimation()
+    {
+        _animator.SetTrigger(nameof(Blow));
+    }
+    private void PlayBlowSound()
+    {
+        _audioSource.Play();
     }
 }
